ce956bb09f98250ab550f2ee3c0d4ae1654d82cb
YiFan <jiao1995cn@gmail.com>
2023-03-24 15:17:32 -0500

    class Score and show Score info created

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,8 @@ const textInput = document.querySelector('.text');
 const score = document.querySelector('.score');
 const time = document.querySelector('.time');
 const start = document.querySelector('.start');
-let times = 5;
+const userInfo = document.querySelector('.user-info');
+
 const music = new Audio('./assets/audio/Love is like fire.mp3');
 music.type = 'audio/mp3';
 
@@ -24,36 +25,65 @@ const wordArray = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot',
 'awesome', 'challenge', 'science', 'mystery', 'famous', 'league', 'memory', 
 'leather', 'planet', 'software', 'update', 'yellow', 'keyboard', 'window']
 
+let scores = 0;
+let i = 0;
+let count = 'timeCount';
 start.addEventListener('click', () => {
-    /* music.play(); */
-    wordArray.sort(() => Math.random() - 0.5);
-    //console.log(time.innerHTML)
-
     if(time.innerHTML.trim() === '--s') {
+        let times = 99;
+        wordArray.sort(() => Math.random() - 0.5);
+        music.play();
+
         titleWord.innerHTML = wordArray[0];
-        const count = setInterval(function(){
+        count = setInterval(function(){
             time.innerHTML = `${times}s`; 
             times--;
-            console.log(times)
             if(times === -1) {
                 clearInterval(count);
+                music.pause();
                 time.innerHTML = 'Timeover'
+                getInfo();
+            }
+        },1000)
+    } else {
+        music.load();
+        clearInterval(count);
+        let times = 99;
+        scores = 0;
+        i = 0;
+        score.innerHTML = '--';
+        wordArray.sort(() => Math.random() - 0.5);
+        titleWord.innerHTML = wordArray[0];
+        music.play();
+        count = setInterval(function(){
+            time.innerHTML = `${times}s`; 
+            times--;
+            if(times === -1) {
+                clearInterval(count);
+                music.pause();
+                time.innerHTML = 'Timeover'
+                getInfo();
             }
         },1000)
     }
-    //console.log(wordArray)
  });
 
- let i = 0;
- let scores = 0;
  textInput.addEventListener('keyup', () => {
-    //console.log(textInput.value)
-    //console.log(wordArray)
-    if(textInput.value === titleWord.innerHTML) {
-        i = ++i;
-        titleWord.innerHTML = wordArray[i];
-        score.innerHTML = `${++scores} points`;
-        textInput.value = '';
+    if(time.innerHTML !== 'Timeover' && score.innerHTML !== 'Congratulations on getting full marks') {
+        if(textInput.value === titleWord.innerHTML) {
+            i = ++i;
+            if(i !== 89) {
+                titleWord.innerHTML = wordArray[i];
+            }
+            score.innerHTML = `${++scores} points`;
+            if(scores === 90) {
+                clearInterval(count);
+                music.pause();
+                score.innerHTML = 'Congratulations on getting full marks';
+                getInfo();
+            }
+            textInput.value = '';
+        }
     }
  })
 
@@ -83,5 +113,17 @@ class Score {
     getInfo () {
         return `${this.getdate} ${this.gethits} ${this.getpercentage}`
     }
+}
+
+function getInfo() {
+    const gMonth = ((new Date()).getMonth()+1).toString().padStart(2, '0');
+    const gDay = (new Date()).getDate().toString().padStart(2, '0');
+    const gYear = (new Date()).getFullYear();
+    const dates = ` ${gMonth} ${gDay}, ${gYear}`;
+    const  percent = `${Math.round(scores / 90 * 10000) / 100.00}%`;
+    const scoreInfo = new Score(dates,scores,percent);
+    scoreInfo.getInfo();
+    const lastInfo = scoreInfo.getInfo().trim().split(' ');
+    userInfo.innerHTML = `Date:${lastInfo[0]} ${lastInfo[1]} ${lastInfo[2]}, Get points:${lastInfo[3]}, The points percentage is:${lastInfo[4]}.`
 }
 
